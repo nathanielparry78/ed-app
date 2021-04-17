613f0ed145d899bc72441d82704377fd58c47aff
nathanielparry78 <nathanielparry@churchofjesuschrist.org>
2021-04-16 21:02:47 -0600

    updated deps

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={main}>
-        <h1 className={title}>
+      <main>
+        <h1>
           Welcome to ED!
         </h1>
 
